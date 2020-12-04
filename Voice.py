cc52d5c3ade84b034cc860d1266021c6f46cb338
Leonardo Almeida de Araújo <leonardo.a@acad.ifma.edu.br>
2020-12-04 19:29:02 -0300

    Atualizado

--- Voice.py
+++ Voice.py
@@ -2,54 +2,24 @@ import speech_recognition as sr
 from gtts import gTTS
 from playsound import playsound
 
-#Funcao responsavel por falar
-
 def cria_audio(audio):
-	tts = gTTS(audio,lang='pt-br')
-
-	#Salva o arquivo de audio
-
-	tts.save('playback/hello.mp3')
-
+	tts = gTTS(audio, lang='pt-br')
+	tts.save('playback/Pb.mp3')
 	print("Estou aprendendo o que você disse...")
-
-	#Da play ao audio
-	playsound('playback/hello.mp3')
-
-
-#Funcao responsavel por ouvir e reconhecer a fala
+	playsound('playback/Pb.mp3')
 
 def ouvir_microfone():
-
-	#Habilita o microfone para ouvir o usuario
 
 	microfone = sr.Recognizer()
 	with sr.Microphone() as source:
 
-		#Chama a funcao de reducao de ruido disponivel na speech_recognition
-
 		microfone.adjust_for_ambient_noise(source)
-
-		#Avisa ao usuario que esta pronto para ouvir
-
-		print("Diga alguma coisa: ")
-
-		#Armazena a informacao de audio na variavel
-
+		print("Microfone Ativo")
 		audio = microfone.listen(source)
 
-
 	try:
-		#Passa o audio para o reconhecedor de padroes do speech_recognition
-
 		frase = microfone.recognize_google(audio, language='pt-BR')
-
-		#Após alguns segundos, retorna a frase falada
-
-		print("Você disse: " + frase)
-
-		#Caso nao tenha reconhecido o padrao de fala, exibe esta mensagem
-
+		print("Frase Falada: " + frase)
 	except sr.UnkownValueError:
 		print("Não entendi")
 
